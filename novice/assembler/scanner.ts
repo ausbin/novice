7f27d7fb3f0b16875846108ed3402e81e8449ec7
Austin Adams <git@austinjadams.com>
2018-08-08 12:23:04 -0400

    Add noop scanner

---
+++ novice/assembler/scanner.ts
@@ -0,0 +1,10 @@
+import { Readable } from 'stream';
+
+class Scanner {
+    public scan(fp: Readable): string[][] {
+        let lines: string[][] = [];
+        return lines;
+    }
+}
+
+export { Scanner };

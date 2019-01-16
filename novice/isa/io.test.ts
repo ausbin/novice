c688b4b37038107e774c49b1fc4b14410b382e90
Austin Adams <git@austinjadams.com>
2019-01-16 15:28:18 -0500

    Add basic tests for StreamIO

---
+++ novice/isa/io.test.ts
@@ -0,0 +1,57 @@
+import { Buffer } from 'buffer';
+import { Readable, Writable } from 'stream';
+import { IO, StreamIO } from './io';
+
+describe('StreamIO', () => {
+    let stdinFp: Readable, stdoutFp: Writable;
+    let stdin: string, stdout: string;
+    let streamIO: IO;
+
+    beforeEach(() => {
+        stdin = stdout = '';
+        // @ts-ignore
+        stdinFp = {
+            read(n: number) {
+                n = Math.min(n, stdin.length);
+                if (!n) {
+                    return null;
+                } else {
+                    const res = Buffer.from(stdin.slice(0, n));
+                    stdin = stdin.slice(n);
+                    return res;
+                }
+            }
+        };
+        // @ts-ignore
+        stdoutFp = {
+            // @ts-ignore
+            write(arr: Uint8Array) {
+                for (let i = 0; i < arr.length; i++)
+                    stdout += String.fromCharCode(arr[i]);
+            }
+        };
+
+        streamIO = new StreamIO(stdinFp, stdoutFp);
+    });
+
+    describe('getc()', () => {
+        it('handles eof', () => {
+            expect(() => {
+                streamIO.getc();
+            }).toThrow('unexpected EOF');
+        });
+
+        it('reads char', () => {
+            stdin = 'hi';
+            expect(streamIO.getc()).toEqual('h'.charCodeAt(0));
+        });
+    });
+
+    describe('putc()', () => {
+        it('writes char', () => {
+            streamIO.putc('x'.charCodeAt(0));
+            streamIO.putc('D'.charCodeAt(0));
+            expect(stdout).toEqual('xD');
+        });
+    });
+});

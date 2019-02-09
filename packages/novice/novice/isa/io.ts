8bc03e6e78d93dc688b639b6b075a7a6e1fc5406
Austin Adams <git@austinjadams.com>
2019-02-09 11:32:03 -0500

    Actually remove StreamIO from novice, whoops

--- packages/novice/novice/isa/io.ts
+++ packages/novice/novice/isa/io.ts
@@ -1,33 +1,6 @@
-import { Readable, Writable } from 'stream';
-
 interface IO {
     getc(): Promise<number>;
     putc(c: number): void;
-}
-
-class StreamIO {
-    private stdin: Readable;
-    private stdout: Writable;
-
-    public constructor(stdin: Readable, stdout: Writable) {
-        this.stdin = stdin;
-        this.stdout = stdout;
-    }
-
-    public async getc(): Promise<number> {
-        const buf = this.stdin.read(1);
-
-        // The in trap provides no way to handle EOFs
-        if (!buf) {
-            throw new Error('unexpected EOF on stdin');
-        }
-
-        return buf[0];
-    }
-
-    public putc(c: number): void {
-        this.stdout.write(new Uint8Array([c]));
-    }
 }
 
 export { IO };
